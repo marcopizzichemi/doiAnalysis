4dea6a76c1e598c150372b811d37fef4150d8ee0
marcopizzichemi <marco.pizzichemi@gmail.com>
2017-08-17 12:02:59 +0200

    added automatic check on crystal numbers

--- doifit.cpp
+++ doifit.cpp
@@ -1,6 +1,6 @@
 //simple analysis
 
-// compile with 
+// compile with
 // g++ -o ../build/doifit doifit.cpp `root-config --cflags --glibs`
 
 
@@ -42,28 +42,22 @@ public:
   double z;
   double s;
   double sqrt_nentries;
-  
-//   void clear()
-//   {
-//     w.clear();
-//     sw.clear();
-//     sqrt_nentries.clear();
-//     z.clear();
-//     sz.clear();
-//   };
-  
+
   friend std::istream& operator>>(std::istream& input, point_t& s)
   {
-    input >> s.x; 
-    input >> s.y;           
-    input >> s.w; 
-    input >> s.z;    
-    input >> s.s; 
+    input >> s.x;
+    input >> s.y;
+    input >> s.w;
+    input >> s.z;
+    input >> s.s;
     input >> s.sqrt_nentries;
     return input;
   }
-  
 };
+
+
+
+
 
 int main (int argc, char** argv)
 {
@@ -74,61 +68,46 @@ int main (int argc, char** argv)
     inputStream << "./" <<  argv[i] << "/doiData.txt";
     file.push_back(inputStream.str());
   }
-  
-  
-//   file.push_back("./z0/doiData.txt");
-//   file.push_back("./z1/doiData.txt");
-//   file.push_back("./z2/doiData.txt");
-//   file.push_back("./z3/doiData.txt");
-//   file.push_back("./z4/doiData.txt");
-//   file.push_back("./z5/doiData.txt");
-//   file.push_back("./z6/doiData.txt");
-//   file.push_back("./z7/doiData.txt");
-//   file.push_back("./z8/doiData.txt");
-//   file.push_back("./z9/doiData.txt");
-  
-//   std::vector<double> z;
-//   z.push_back(13.6);
-//   z.push_back(10.8);
-//   z.push_back(8);
-//   z.push_back(5.2);
-//   z.push_back(2.4);
-  
+
   std::ifstream data[file.size()];
   int x,y;
   double w,z,s,sqrt_nentries;
+  std::vector<int> counter;
+
   for(int i = 0 ; i < file.size() ; i++)
   {
     data[i].open(file[i].c_str());
-    int counter = 0;
+    int fileCounter = 0;
     point_t pointData;
     while(data[i] >> pointData)
     {
-//       data[i] >> x >> y >> w >> z >> s >> sqrt_nentries;
-      Crystal[counter].x.push_back(pointData.x);
-      Crystal[counter].y.push_back(pointData.y);
-      Crystal[counter].w.push_back(pointData.w);
-      Crystal[counter].z.push_back(pointData.z);
-      Crystal[counter].s.push_back(pointData.s);
-      Crystal[counter].sqrt_nentries.push_back(pointData.sqrt_nentries);
-      counter++;
+      Crystal[fileCounter].x.push_back(pointData.x);
+      Crystal[fileCounter].y.push_back(pointData.y);
+      Crystal[fileCounter].w.push_back(pointData.w);
+      Crystal[fileCounter].z.push_back(pointData.z);
+      Crystal[fileCounter].s.push_back(pointData.s);
+      Crystal[fileCounter].sqrt_nentries.push_back(pointData.sqrt_nentries);
+      fileCounter++;
+    }
+    counter.push_back(fileCounter);
+  }
+
+  const int NumOfCrystals = counter[0];
+  for(int i = 1 ; i < counter.size() ; i++)
+  {
+    if (counter[i] != NumOfCrystals)
+    {
+      std::cout << "ERROR: doiData.txt files have different number of rows!" << std::endl;
+      std::cout << "Aborting" << std::endl;
+      return -1;
     }
   }
-  
-  
-//   for(int i = 0 ; i < file.size() ; i++)
-//   {
-//     int counter = 0;
-//     for(int j = 0 ; j < 8 ; j++)
-//     {
-//       std::cout <<  Crystal[counter].x[i] << " "<<  Crystal[counter].y[i] << " " << Crystal[counter].w[i] << " " << Crystal[counter].s[i] << std::endl;
-//       counter++;
-//     }
-//     std::cout << std::endl;
-//   }
-  
+
+
+
+
   //calc average sigma w peak (multiply first by sqrt_nentries)
-  for(int i = 0 ; i < 8 ; i++)
+  for(int i = 0 ; i < NumOfCrystals ; i++)
   {
     double partial = 0;
     for(int j = 0 ; j < Crystal[i].s.size(); j++)
@@ -137,27 +116,27 @@ int main (int argc, char** argv)
     }
     Crystal[i].avgs = partial/Crystal[i].s.size();
   }
-  
-  
+
+
   std::stringstream tempStream;
-  
-  for(int j = 0 ; j < 8 ; j++)
+
+  for(int j = 0 ; j < NumOfCrystals ; j++)
   {
     std::cout << "Correlation Plot" << std::endl;
     tempStream.str("");
     tempStream << "Canvas_DOIvsR_" << Crystal[j].x[0] << "_" << Crystal[j].y[0];
-    
+
     Crystal[j].Cplot = new TCanvas(tempStream.str().c_str(),"",800,600);
     tempStream << ".png";
     std::string plotFile = tempStream.str().c_str();
-    
+
     std::vector<double> tempEy,tempEx;
     for(int tt = 0 ; tt < file.size()  ;tt++)
     {
       tempEy.push_back(0.5);
 //       tempEx.push_back( Crystal[tt].s[j]);
     }
-    
+
     //Crystal[j].doiR = new TGraphErrors(5,&Crystal[j].x[0],&Crystal[j].y[0],&Crystal[j].ex[0],&Crystal[j].ey[0]);
     Crystal[j].doiR = new TGraphErrors(file.size(),&Crystal[j].w[0],&Crystal[j].z[0],&Crystal[j].s[0],&tempEy[0]);
     Crystal[j].doiR->GetXaxis()->SetTitle("W");
@@ -167,7 +146,7 @@ int main (int argc, char** argv)
     Crystal[j].doiR->SetTitle("");
     Crystal[j].Cplot->cd();
     Crystal[j].doiR->Draw("A*");
-    
+
     TF1 *fit = new TF1("fit", "[0]+x*[1]",0,1);
     fit->SetParameter(0,80);
     fit->SetParameter(1,-100);
@@ -175,7 +154,7 @@ int main (int argc, char** argv)
     fit->SetLineColor(2);
     Crystal[j].Cplot->cd();
     fit->Draw("same");
-    
+
     Crystal[j].Cplot->Print(plotFile.c_str());
 //     Crystal[j].fitPar0 = fit->GetParameter(0);
 //     Crystal[j].fitPar1 = fit->GetParameter(1);
@@ -183,32 +162,32 @@ int main (int argc, char** argv)
     Crystal[j].q = fit->GetParameter(0);
     Crystal[j].doires = std::abs(fit->GetParameter(1) * Crystal[j].avgs *2.355);
     std::cout << "Crystal " << j << " = " << fit->GetParameter(1) << " , " << Crystal[j].doires<< std::endl;
-    
-    
-    
+
+
+
   }
-  
+
   std::ofstream calib;
   calib.open("calibration_params.txt");
-  
-  for(int j = 7 ; j > -1 ; j--)
+
+  for(int j = NumOfCrystals - 1 ; j > -1 ; j--)
   {
-    calib << Crystal[j].x[0] << "\t" 
-          << Crystal[j].y[0] << "\t" 
+    calib << Crystal[j].x[0] << "\t"
+          << Crystal[j].y[0] << "\t"
 	  << Crystal[j].m << "\t"
 	  << Crystal[j].q << "\t"
 	  << Crystal[j].doires << "\t"
 	  << Crystal[j].avgs << "\t";
-    for(int i = 0 ; i < Crystal[j].w.size(); i++) 
+    for(int i = 0 ; i < Crystal[j].w.size(); i++)
       calib << Crystal[j].w[i] << "\t"<< Crystal[j].s[i] << "\t" <<  Crystal[j].sqrt_nentries[i] << "\t";
     calib << std::endl;
   }
   calib.close();
-  
-  
+
+
   std::ofstream doi;
   doi.open("doi.txt");
-  for(int j = 7 ; j > -1 ; j--)
+  for(int j = NumOfCrystals ; j > -1 ; j--)
   {
     doi << Crystal[j].x[0] << "\t" << Crystal[j].y[0] << "\t" ;
     doi << Crystal[j].doires << "\t" << Crystal[j].avgs <<std::endl;
